67e00edd467b754f881dd7ed25deeea92e89a38e
NagaSwathiAmbati07 <nagaswathi.ambati07@gmail.com>
2024-02-01 11:28:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import ChatBot from '~/screens/chat-bot'
-import { ChatBotProvider } from './src/providers/chatbot-provider'
+import React from "react";
+import ChatBot from "~/screens/chat-bot";
+import { ChatBotProvider } from "./src/providers/chatbot-provider";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App () {
-    return (
-        <ChatBotProvider>
-            <ChatBot />
-        </ChatBotProvider>
-    )
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <ChatBotProvider>
+        <ChatBot />
+      </ChatBotProvider>
+    </SafeAreaProvider>
+  );
 }
